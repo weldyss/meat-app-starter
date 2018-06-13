3a10801a8e2047ac2287b994f7c10d8effb6bbb1
Weldys Santos <weldyss@gmail.com>
2018-06-13 16:53:42 -0300

    all the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -8,7 +8,16 @@ import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component'
+import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './menu/menu.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { ShoppingCartService } from './shopping-cart/shopping-cart.service';
 
 
 @NgModule({
@@ -16,14 +25,21 @@ import { AboutComponent } from './about/about.component'
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    RestaurantsComponent,
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [RestaurantsService, ShoppingCartService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,19 @@ import { Routes } from '@angular/router';
 
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { MenuComponent } from './menu/menu.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent},
+    { path: 'restaurants', component: RestaurantsComponent},
+    { path: 'restaurants/:id', component: RestaurantDetailComponent,
+        children: [
+            {path: '', redirectTo: 'menu', pathMatch: 'full'},
+            { path: 'menu', component: MenuComponent},
+            { path: 'reviews', component: ReviewsComponent}
+        ]},
     { path: 'about', component: AboutComponent}
 ]
