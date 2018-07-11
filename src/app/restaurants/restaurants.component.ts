146a8852441c7f49d5ab42cb671e8ec87ed5955e
Weldys Santos <weldyss@gmail.com>
2018-07-10 23:31:57 -0300

    search

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -4,7 +4,12 @@ import { RestaurantsService } from './restaurants.service';
 import { trigger, animate, state, style, transition } from '@angular/animations';
 import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 
-import 'rxjs/operator/switchMap'
+import 'rxjs/add/operator/switchMap'
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
+import { Observable } from "rxjs/Observable";
 
 @Component({
   selector: 'mt-restaurants',
@@ -40,10 +45,16 @@ export class RestaurantsComponent implements OnInit {
       searchControl: this.searchControl
     })
     this.searchControl.valueChanges
+    .debounceTime(500)
+    .distinctUntilChanged()
     .switchMap(searchTerm => 
-      this.restaurantsService.restaurants(searchTerm)).subscribe(
+      this.restaurantsService.restaurants(searchTerm)
+      .catch(error => Observable.from([]))).subscribe(
         restaurants => this.restaurants = restaurants
       )
+    this.restaurantsService.restaurants().subscribe(
+      restaurants => this.restaurants = restaurants
+    )
   }
 
   toggleSearch() {
