58dbabd3db116ebae7a3b44cf3709388a6033e46
Weldys Santos <weldyss@gmail.com>
2018-07-21 22:57:30 -0300

    configs from ts and server.js

--- backend/user.ts
+++ backend/user.ts
@@ -2,7 +2,7 @@ export class User {
     constructor(public email: string, public name: string, private password: string) {}
 
     matches(another: User): boolean {
-        return another !== undefined && another.email == this.email && another.password == this.password
+        return another !== undefined && another.email === this.email && another.password === this.password
     }
 }
 
